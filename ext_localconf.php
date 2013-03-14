316100d8f0a3a532a8222870691aebe85502fe96
Jozef Spisiak <jozef@pixelant.se>
2013-03-14 10:58:16 +0100

    Cleanup unused code and upgrade of lessphp to 0.3.9

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,9 @@
+
 <?php
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = 'EXT:ws_less/Classes/Hooks/RenderPreProcessorHook.php:&tx_Wsless_Hooks_RenderPreProcessorHook->renderPreProcessorProc';
-
-#$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rtehtmlarea/class.tx_rtehtmlarea_base.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/XClass/class.ux_tx_rtehtmlarea_base.php';
-
-
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,5 +3,4 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-//t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Less configuration');
 ?>
