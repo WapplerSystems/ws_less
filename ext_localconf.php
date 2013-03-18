896db114c13e9137d6d7afdca1c03f3351d31d2c
Jozef Spisiak <jozef@pixelant.se>
2013-03-18 09:17:23 +0100

    [FEATURE] implemented caching framework
    Caching framework has been implemented regenerating the compiled CSS
    files only when there has been changes in them or files included.

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,4 +6,15 @@ if (!defined('TYPO3_MODE')) {
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = 'EXT:ws_less/Classes/Hooks/RenderPreProcessorHook.php:&tx_Wsless_Hooks_RenderPreProcessorHook->renderPreProcessorProc';
 
+// Caching the 404 pages - default expire 3600 seconds
+if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_less'])) {
+	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_less'] = array(
+		'frontend' => 't3lib_cache_frontend_VariableFrontend',
+		'backend' => 't3lib_cache_backend_FileBackend',
+		'options' => array(
+				'defaultLifetime' => 3600*24*7,
+			),
+	);
+}
+
 ?>
