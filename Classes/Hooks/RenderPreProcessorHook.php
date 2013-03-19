144ae929e41a602b09b880e0c67b3c439627edae
Sven Wappler <info@wapplersystems.de>
2013-03-19 08:31:17 +0100

    Added Jozef Spisiak as coauthor

--- Classes/Hooks/RenderPreProcessorHook.php
+++ Classes/Hooks/RenderPreProcessorHook.php
@@ -24,6 +24,9 @@
 
 /**
  * Hook to preprocess less files
+ * 
+ * @author Sven Wappler <typo3YYYY@wapplersystems.de>
+ * @author Jozef Spisiak <jozef@pixelant.se>
  *
  */
 class tx_Wsless_Hooks_RenderPreProcessorHook {
@@ -40,7 +43,7 @@ class tx_Wsless_Hooks_RenderPreProcessorHook {
 	 */
 	public function renderPreProcessorProc(&$params, $pagerenderer) {
 
-		$this->parser = new lessc();
+		$this->parser = t3lib_div::makeInstance('lessc');
 
 		if (!is_array($params['cssFiles'])) return;
 
@@ -49,37 +52,50 @@ class tx_Wsless_Hooks_RenderPreProcessorHook {
 		foreach ($params['cssFiles'] as $file => $conf) {
 			$pathinfo = pathinfo($conf['file']);
 			
-			if ($pathinfo['extension'] == 'less') {
-
-				$outputdir = "typo3temp";
-
-				// search settings for less file
-				foreach ($GLOBALS['TSFE']->pSetup['includeCSS.'] as $key => $subconf) {
-					if ($GLOBALS['TSFE']->pSetup['includeCSS.'][$key] == $file) {
-						if (isset($GLOBALS['TSFE']->pSetup['includeCSS.'][$key . '.']['outputdir'])) $outputdir = rtrim($GLOBALS['TSFE']->pSetup['includeCSS.'][$key . '.']['outputdir'],"/");
-					}
-				}
-
-				$lessFilename = t3lib_div::getFileAbsFileName($conf['file']);
-
-				$cssRelativeFilename = $outputdir."/".$pathinfo['filename'].".css";
-				$cssFilename = PATH_site.$cssRelativeFilename;
-
-				t3lib_div::mkdir_deep(PATH_site.$outputdir."/");
-
-				try {
-					$this->compileScss($lessFilename, $cssFilename);
-				} catch (Exception $ex) {
-					// log the exception to the TYPO3 log as error
-				}
-
-				$cssFiles[$cssRelativeFilename] = $params['cssFiles'][$file];
-				$cssFiles[$cssRelativeFilename]['file'] = $cssRelativeFilename;
-			}
-			else
-			{
+			if ($pathinfo['extension'] !== 'less') {
 				$cssFiles[$file] = $conf;
+				continue;
 			}
+
+			$outputdir = "typo3temp/ws_less";
+
+			// search settings for less file
+			foreach ($GLOBALS['TSFE']->pSetup['includeCSS.'] as $key => $subconf) {
+				if ($GLOBALS['TSFE']->pSetup['includeCSS.'][$key] == $file) {
+					if (isset($GLOBALS['TSFE']->pSetup['includeCSS.'][$key . '.']['outputdir'])) $outputdir = rtrim($GLOBALS['TSFE']->pSetup['includeCSS.'][$key . '.']['outputdir'],"/");
+				}
+			}
+
+			$lessFilename = t3lib_div::getFileAbsFileName($conf['file']);
+
+			// create filename - hash is importand due to the possible conflicts with same filename in different folder
+			t3lib_div::mkdir_deep(PATH_site.$outputdir."/");
+			$cssRelativeFilename = $outputdir."/".$pathinfo['filename']."_".hash('sha1',$file).".css";
+			$cssFilename = PATH_site.$cssRelativeFilename;
+
+			$cache = $GLOBALS['typo3CacheManager']->getCache('ws_less');
+			$cacheKey = hash('sha1',$cssRelativeFilename);
+			$contentHash = $this->calculateContentHash($lessFilename);
+			$contentHashCache = '';
+			if ($cache->has($cacheKey))
+			{
+				$contentHashCache = $cache->get($cacheKey);
+			}
+
+			try {
+				if ($contentHashCache == '' || $contentHashCache != $contentHash)
+				{
+					$this->compileScss($lessFilename, $cssFilename);
+				}
+			} catch (Exception $ex) {
+				// log the exception to the TYPO3 log as error
+				t3lib_div::sysLog($ex->getMessage(), t3lib_div::SYSLOG_SEVERITY_ERROR);
+			}
+
+			$cache->set($cacheKey,$contentHash,array());
+
+			$cssFiles[$cssRelativeFilename] = $params['cssFiles'][$file];
+			$cssFiles[$cssRelativeFilename]['file'] = $cssRelativeFilename;
 		}
 		$params['cssFiles'] = $cssFiles;
 	}
@@ -103,6 +119,31 @@ class tx_Wsless_Hooks_RenderPreProcessorHook {
 			}
 		}
 		return $cssFilename;
+	}
+
+	/**
+	 * Calculating content hash to detect changes
+	 *
+	 * @param string $lessFilename Existing less file absolute path
+	 * @return string
+	 *
+	 */
+	protected function calculateContentHash($lessFilename) {
+		$content = file_get_contents($lessFilename);
+		$hash = hash('sha1',$content);
+
+		$matched = preg_match_all('/@import "([^"])*"/', $content, $imports);
+		if (!empty($imports[0]))
+			$pathinfo = pathinfo($lessFilename);
+
+		foreach ($imports[0] as $import)
+		{
+			$importFilename = str_replace("@import", '', $import);
+			$importFilename = trim(str_replace("\"", '', $importFilename));
+			$hash = hash('sha1', $hash . sha1_file($pathinfo['dirname'] . '/' . $importFilename));
+		}
+
+		return $hash;
 	}
 }
 ?>
