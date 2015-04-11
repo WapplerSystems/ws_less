c3dd5d656f21e931fccd79de0ed07a168310861b
Sven Wappler <info@wapplersystems.de>
2015-04-12 01:03:31 +0200

    Now compatible to TYPO3 7.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Compiles less files to CSS files.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.4.0',
+	'version' => '1.4.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.1.0-6.2.99',
+			'typo3' => '6.1.0-7.1.99',
 		),
 		'conflicts' => array(
 		),
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = 'EXT:ws_less/Classes/Hooks/RenderPreProcessorHook.php:&\WapplerSystems\WsLess\Hooks\RenderPreProcessorHook->renderPreProcessorProc';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = 'EXT:ws_less/Classes/Hooks/RenderPreProcessorHook.php:&WapplerSystems\WsLess\Hooks\RenderPreProcessorHook->renderPreProcessorProc';
 
 // Caching the pages - default expire 3600 seconds
 if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_less'])) {
