d35906413cba9b7e7ee0cfbd004d8fdc5e6ac16f
Oliver Schlöbe <github@schloebe.de>
2020-02-16 19:18:52 +0100

    TYPO3 v9 Compatibility Changes

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = 'EXT:ws_less/Classes/Hooks/RenderPreProcessorHook.php:&WapplerSystems\WsLess\Hooks\RenderPreProcessorHook->renderPreProcessorProc';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = 'WapplerSystems\WsLess\Hooks\RenderPreProcessorHook->renderPreProcessorProc';
 
 // Caching the pages - default expire 3600 seconds
 if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_less'])) {
@@ -11,7 +11,7 @@ if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_less
 		'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
 		'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\FileBackend',
 		'options' => array(
-            'defaultLifetime' => 3600*24*7,
+			'defaultLifetime' => 3600*24*7
         ),
 	);
 }
