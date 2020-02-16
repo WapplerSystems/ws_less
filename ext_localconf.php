01fefeefe07a92fa2d7c63a411025afc2d71454e
Oliver Schlöbe <github@schloebe.de>
2020-02-16 20:16:43 +0100

    Further Improve Hook Registration And Bootstrapping

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,20 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = 'WapplerSystems\WsLess\Hooks\RenderPreProcessorHook->renderPreProcessorProc';
+$boot = function () {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = \WapplerSystems\WsLess\Hooks\RenderPreProcessorHook::class . '->renderPreProcessorProc';
 
-// Caching the pages - default expire 3600 seconds
-if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_less'])) {
-	$TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['ws_less'] = array(
-		'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-		'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\FileBackend',
-		'options' => array(
-			'defaultLifetime' => 3600*24*7
-        ),
-	);
-}
+	// Caching the pages - default expire 3600 seconds
+	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ws_less'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ws_less'] = array(
+			'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+			'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\FileBackend',
+			'options' => array(
+				'defaultLifetime' => 3600*24*7
+			),
+		);
+	}
+};
 
+$boot();
+unset($boot);
