d6bd3582646182e8831f897d3802408a790a0d00
Sven Wappler <info@wappler.systems>
2020-11-16 10:40:26 +0100

    composer.json dependencies updated

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-            'php' => '7.2.0-7.4.99'
+            'php' => '7.2.0-7.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
