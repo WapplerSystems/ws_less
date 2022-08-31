fa31eddf058e3c5361e5c62465a2ddbc48d5fe71
Sven Wappler <info@wappler.systems>
2022-09-01 00:32:26 +0200

    avoid undefined array key

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@ $boot = function () {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['wsless'] = \WapplerSystems\WsLess\Hooks\RenderPreProcessorHook::class.'->renderPreProcessorProc';
 
     // Caching the pages - default expire 3600 seconds
-    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ws_less'])) {
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ws_less'] ?? null)) {
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ws_less'] = [
             'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
             'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
